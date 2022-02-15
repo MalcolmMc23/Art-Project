f839f0814ca2c3203208d2e25d44a4923c062ce8
MM810828 <91161786+MM810828@users.noreply.github.com>
2022-02-15 08:48:38 -0800

    stuff

---
+++ Art Project 1/sketch.js
@@ -0,0 +1,13 @@
+// Malcolm McDonald
+// 2/15/22
+// Art Project 1
+
+function setup() {
+  var cnv = createCanvas(1618., 1000 );
+  cnv.position((windowWidth-width)/2, 30);
+
+}
+
+function draw(){
+  background(20,20,20,20)
+}
