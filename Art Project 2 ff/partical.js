0e051f2649c9d960ba9fb1a57224ab7c0459d6bc
MM810828 <91161786+MM810828@users.noreply.github.com>
2022-02-16 08:48:45 -0800

    adding ff

---
+++ Art Project 2 ff/partical.js
@@ -0,0 +1,54 @@
+class Partical {
+        constructor() {
+            this.loc = createVector(random(width), random(height));
+            this.vel = createVector(0, 0)
+            this.acc = createVector(0, 0)
+            this.diam = 5
+            this.lim = 0.5;
+            this.num = floor(random(1, 2));
+        }
+    
+        run() {
+            this.render();
+            this.checkEdges()
+        }
+    
+        render() {
+            if(this.num === 1){
+                this.clr = color(255,0,0)
+            } else {
+                this.clr = color(0, 0, 255)
+            }
+    
+            fill(this.clr);
+            noStroke(.01)
+            ellipse(this.loc.x, this.loc.y, this.diam);
+        }
+        update() {
+            this.loc.add(this.vel);
+            this.vel.add(this.acc);
+            this.acc.mult(0)
+            this.vel.limit(this.lim)
+        }
+    
+        checkEdges() {
+        if (this.loc.x < 0) this.loc.x = width;
+        if (this.loc.x > width) this.loc.x = 0;
+        if (this.loc.y < 0) this.loc.y = height;
+        if (this.loc.y > height) this.loc.y = 0; 
+        }
+
+        follow(vectors) {
+            var x = floor(this.loc.x / cellSize);
+            var y = floor(this.loc.y / cellSize);
+            var index = x + y * cols;
+            var force = vectors[index];
+            this.applyForce(force);
+
+        }
+        
+
+        applyForce(force) {
+            this.acc.add(force)
+        }
+}
---
+++ Art Project 2 ff/sketch.js
@@ -0,0 +1,78 @@
+// Malcolm McDonald
+// 1/25/22
+//FlowField
+
+var inc = 0.1;
+var start = 0;
+var cellSize = 50
+var cols, rows;
+var zoff = 0;
+var fr;
+var particals = [];
+var flowField;
+
+
+function setup() {
+  var cnv = createCanvas(700, 700);
+  cnv.position((windowWidth-width)/2, 30);
+  // background(20)
+
+  fr = createP('')
+  cols = floor(width/cellSize);
+  rows = floor(height/cellSize);
+
+  flowField = new Array(cols * rows)
+
+  for(var i = 0; i < 200; i++) {
+  particals[i] = new Partical();
+  }
+
+
+
+}
+
+function draw() {
+  background(20, 20, 20, 50);
+
+  var yoff = 0;
+  for(var x = 0; x < rows; x++) {
+    var xoff = 0;
+    for(var y = 0; y < cols; y++) {
+      index = x + y * cols
+      var r = noise(xoff, yoff, zoff) * TWO_PI
+      var v = p5.Vector.fromAngle(r);
+      v.setMag(0.1)
+      flowField[index] = v
+      xoff += inc;
+      stroke(0, 100);
+      push();
+      translate(x * cellSize, y * cellSize);
+      rotate(v.heading())
+      // line(0, 0, cellSize, 0)
+
+      pop();
+      // fill(r);
+      // rect(x * cellSize, y * cellSize, cellSize);
+    }
+    yoff += inc;
+
+    zoff += 0.001;
+
+  for(var i = 0; i < particals.length; i++) {
+    particals[i].follow(flowField);
+    particals[i].run();
+    particals[i].update();
+    }
+
+    fr.html(floor(frameRate()));
+  }
+}
+
+
+// function swapPixels() {
+//   for(var i = 0; i < img.width; i++) {
+//     for(var j = 0; j < img.height; j++) {
+
+//     }
+//   }
+// }
